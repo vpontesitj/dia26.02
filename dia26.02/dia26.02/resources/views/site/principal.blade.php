433c1041e1df30a5fbc3f0975da35eb31e100017
vpontesitj <maria.almeida442@etec.sp.gov.br>
2025-02-26 12:42:41 -0300

    oi

---
+++ dia26.02/dia26.02/resources/views/site/principal.blade.php
@@ -0,0 +1,16 @@
+<h3> Principal (view) </h3>
+
+<ul>
+     <li>
+        <a href ="{{ route('site.index') }}"> Principal</a>
+</li>
+
+<li>
+        <a href ="{{ route('site.sobrenos') }}"> Sobre nos</a>
+</li> 
+
+<li>
+        <a href ="{{ route('site.contato') }}"> contato</a>
+</li>
+
+</ul>
